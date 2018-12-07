ec92d78a83a6dcab9619b75319b1d7063bfb6015
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-07 12:56:06 +0100

    add router module to note module

--- src/app/modules/admin/modules/note/note.module.ts
+++ src/app/modules/admin/modules/note/note.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NotesComponent } from './components/notes/notes.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [NotesComponent]
 })
