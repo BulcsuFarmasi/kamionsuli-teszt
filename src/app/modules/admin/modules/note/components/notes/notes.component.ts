89e7b43bc58143f4c0119f0080ce59cdea859e96
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-07 12:15:22 +0100

    add notes component markup

--- src/app/modules/admin/modules/note/components/notes/notes.component.ts
+++ src/app/modules/admin/modules/note/components/notes/notes.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { Note } from 'src/app/models/note';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  notes:Note[];
+  private notesSubsciption:Subscription;
+  
+  constructor(private noteService:NoteService) { }
 
   ngOnInit() {
+    this.notesSubsciption = this.noteService.getNotes(0).subscribe(notes => {this.notes = notes});
+  }
+
+  ngOnDestroy() {
+    this.notesSubsciption.unsubscribe();
   }
 
 }
