f767c1941691cb071b029a3dea70146784b8c796
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-07 13:11:36 +0100

    add delete method to service

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { Group } from '../models/group';
 import { Note } from '../models/note';
 import { NetworkService } from './network.service';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class NoteService {
 
   constructor(private networkService:NetworkService) {}
 
+  deleteNote (id:number) {
+		return this.networkService.delete('note/' + id)
+			.pipe(
+				map((response:any) => {
+					if (response.errorCode) {
+						throwError(response);
+					}
+				}
+			)
+		)
+	}
+  
   getNotes (groupTypeId:number):Observable<Note[]> {
     return <Observable<Note[]>> this.networkService.get('notes/' + groupTypeId)
            .pipe(map((notes:Note[]) => {
