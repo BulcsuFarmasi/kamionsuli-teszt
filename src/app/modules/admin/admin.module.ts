1e6f72a875840347264c13e003025b5aa283e811
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-07 12:55:00 +0100

    add note module to admin module

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -7,13 +7,14 @@ import { AdminComponent } from './components/admin/admin.component';
 import { AdminRoutingModule } from './modules/admin-routing/admin-routing.module';
 import { TestModule } from './modules/test/test.module';
 import { FillModule } from './modules/fill/fill.module';
+import { NoteModule } from './modules/note/note.module';
 import { UserModule } from '../user/user.module';
 import { GroupModule } from './modules/group/group.module';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
  
 @NgModule({
-	imports:[CommonModule, AdminRoutingModule, TestModule, FillModule, GroupModule, UserModule], 
+	imports:[CommonModule, AdminRoutingModule, TestModule, FillModule, GroupModule, NoteModule, UserModule], 
 	declarations:[AdminComponent, NavBarComponent],
 	exports: [AdminComponent]
 
