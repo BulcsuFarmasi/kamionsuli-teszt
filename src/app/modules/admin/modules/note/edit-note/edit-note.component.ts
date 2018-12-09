430a78875a0a23040fd9a8e9073c666cc847240e
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-09 16:07:23 +0100

    get note in component

--- src/app/modules/admin/modules/note/edit-note/edit-note.component.ts
+++ src/app/modules/admin/modules/note/edit-note/edit-note.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { Note } from 'src/app/models/note';
+import { NoteService } from 'src/app/services/note.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-edit-note',
+  selector: 'edit-note',
   templateUrl: './edit-note.component.html',
   styleUrls: ['./edit-note.component.scss']
 })
-export class EditNoteComponent implements OnInit {
+export class EditNoteComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  note:Note;
+  noteSubscription:Subscription;
+  
+  constructor(private noteService:NoteService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.noteSubscription = this.noteService.getNote(id).subscribe(note => {
+      this.note = note;
+    })
+  }
+
+  ngOnDestroy () {
+    this.noteSubscription.unsubscribe();
   }
 
 }
