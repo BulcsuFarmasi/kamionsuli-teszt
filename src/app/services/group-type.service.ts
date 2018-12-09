30e5b6844119802e8edb852e6c448b2cbf92bdb0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-12-09 08:06:39 +0100

    get group types

--- src/app/services/group-type.service.ts
+++ src/app/services/group-type.service.ts
@@ -3,11 +3,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { GroupType } from '../models/group-type';
+import { NetworkService } from './network.service';
 
 @Injectable()
 export class GroupTypeService {
 
-  constructor(private networkService) { }
+  constructor(private networkService:NetworkService) { }
 
   getGroupTypes ():Observable<GroupType[]> {
     return <Observable<GroupType[]>> this.networkService.get('groupTypes');
